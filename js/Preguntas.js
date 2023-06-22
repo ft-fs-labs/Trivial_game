11ffb3828f5fb651cb41dcdc469bd7cb176fefcd
carlosaj98 <carlosaj.arahal@gmail.com>
2023-06-22 15:41:30 +0200

    Preguntas.js creado

---
+++ js/Preguntas.js
@@ -0,0 +1,20 @@
+const containerDOM = document.getElementById("main-container")
+class Pregunta {
+    constructor() {
+        this.btnPregunta = document.createElement("button")
+        this.btnPregunta.innerHTML= "Nueva Pregunta"
+    }
+
+    start() {
+        containerDOM.appendChild(this.btnPregunta)
+        this.btnPregunta.addEventListener("click", this.load)
+    }
+
+    load(){
+        const pantallaFinal = new PantallaFinal()
+        pantallaFinal.start()
+    }
+
+}
+const pregunta = new Pregunta()
+pregunta.start()
