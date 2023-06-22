e57559c6afa64450ab034f71d0c651eeb1e24b7d
vegam91 <vegamartinoj@gmail.com>
2023-06-22 15:48:43 +0200

    cambios

---
+++ start.js
@@ -0,0 +1,22 @@
+
+
+
+class start{
+constructor(){
+    this.startBoton = document.createELement('button')
+    this.startBoton.innerText = 'EMPEZAR'
+    document.body.appendChild(this.startBoton)
+    // this.startBoton.addEventListener('click',handleInit)
+}
+ 
+handleInit(){
+}
+
+
+}
+
+let boton = new start()
+
+
+
+
